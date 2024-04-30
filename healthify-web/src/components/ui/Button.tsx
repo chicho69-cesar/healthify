63a42bd2d401c5807f2742cb55cbe151ba30a7a6
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-04-29 21:19:16 -0600

    :tada: Update button ui component

--- healthify-web/src/components/ui/Button.tsx
+++ healthify-web/src/components/ui/Button.tsx
@@ -7,14 +7,14 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode | React.ReactNode[] | JSX.Element | JSX.Element[]
 }
 
-export function Button({ children, className, variant, ...rest }: Props) {
+export function Button({ children, className, variant = 'primary', ...rest }: Props) {
   return (
     <button
       {...rest}
       className={clsx(
-        'flex h-10 items-center justify-center rounded-lg bg-sky-600 px-4 text-base font-medium text-white transition-colors hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600 active:bg-sky-700 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
+        'flex h-10 items-center justify-center rounded-lg px-4 text-base font-medium text-white transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
         className,
-        variant === 'secondary' && 'bg-red-600 text-white hover:bg-red-500 active:bg-red-700 focus-visible:outline-red-600',
+        variant === 'primary' ? 'bg-sky-600 hover:bg-sky-500 active:sky-red-700 focus-visible:outline-sky-600' : 'bg-red-600 hover:bg-red-500 active:bg-red-700 focus-visible:outline-red-600',
       )}
     >
       {children}
